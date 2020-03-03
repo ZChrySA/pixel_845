9b56e3384e677f0050838131fdc60abdf4275526
dianlujitao <dianlujitao@lineageos.org>
2020-03-04 00:16:48 +0800

    sdm660-common: Switch to qti vibrator HAL
    Change-Id: I2ac3fb7ab15eb316999427697d97dd403d5c7ef1
    
    lavender: nuke vibrator 1.0 from manifest
    
    Signed-off-by: alanndz <alanmahmud0@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- device.mk
+++ device.mk
@@ -566,8 +566,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
 
 # WLAN driver configuration files
 PRODUCT_COPY_FILES += \
