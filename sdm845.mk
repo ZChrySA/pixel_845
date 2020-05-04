2778233d774c858cb3d1075c55ec6c4b400c2ca4
Ethan Halsall <ethan.halsall@gmail.com>
2020-05-04 19:11:17 +0100

    sdm845-common: do not use mpss data for now
    - This is tempory rpmh is not reporting mpss data for some reason
    - Todo revert when there is a fix
    
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -289,7 +289,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.dipper-libperfmgr
+    android.hardware.power@1.3-service.dipper-libperfmgr \
     android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_COPY_FILES += \
