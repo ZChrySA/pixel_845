79e9598d936f60427926f2dfaf6170e725fe7299
Dyneteve <dyneteve@hentaios.com>
2020-08-15 16:46:34 +0200

    powerstats: Move power.stats from common manifest to separate one
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>

--- sdm845.mk
+++ sdm845.mk
@@ -294,7 +294,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.dipper-libperfmgr \
-    android.hardware.power.stats@1.0-service.xiaomi
+    android.hardware.power.stats@1.0-service.dipper
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
