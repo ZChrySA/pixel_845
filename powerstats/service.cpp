1cac23cba477eaa1eb917c2622411ab0983dc392
Arseniy Graur <argraur@gmail.com>
2020-08-15 15:07:15 +0200

    powerstats: Remove unsupported power entities
    Signed-off-by: Arseniy Graur <argraur@gmail.com>

--- powerstats/service.cpp
+++ powerstats/service.cpp
@@ -48,12 +48,9 @@ using android::hardware::google::pixel::powerstats::AidlStateResidencyDataProvid
 using android::hardware::google::pixel::powerstats::GenericStateResidencyDataProvider;
 using android::hardware::google::pixel::powerstats::PowerEntityConfig;
 using android::hardware::google::pixel::powerstats::StateResidencyConfig;
-using android::hardware::google::pixel::powerstats::WlanStateResidencyDataProvider;
 
 int main(int /* argc */, char ** /* argv */) {
     ALOGI("power.stats service 1.0 is starting.");
-
-    bool isDebuggable = android::base::GetBoolProperty("ro.debuggable", false);
 
     PowerStats *service = new PowerStats();
 
@@ -76,9 +73,6 @@ int main(int /* argc */, char ** /* argv */) {
 
     uint32_t apssId = service->addPowerEntity("APSS", PowerEntityType::SUBSYSTEM);
     rpmSdp->addEntity(apssId, PowerEntityConfig("APSS", rpmStateResidencyConfigs));
-
-    //uint32_t mpssId = service->addPowerEntity("MPSS", PowerEntityType::SUBSYSTEM);
-    //rpmSdp->addEntity(mpssId, PowerEntityConfig("MPSS", rpmStateResidencyConfigs));
 
     uint32_t adspId = service->addPowerEntity("ADSP", PowerEntityType::SUBSYSTEM);
     rpmSdp->addEntity(adspId, PowerEntityConfig("ADSP", rpmStateResidencyConfigs));
@@ -116,56 +110,8 @@ int main(int /* argc */, char ** /* argv */) {
 
     service->addStateResidencyDataProvider(socSdp);
 
-    if (isDebuggable) {
-        // Add WLAN power entity
-        uint32_t wlanId = service->addPowerEntity("WLAN", PowerEntityType::SUBSYSTEM);
-        sp<WlanStateResidencyDataProvider> wlanSdp =
-            new WlanStateResidencyDataProvider(wlanId, "/d/wlan0/power_stats");
-        service->addStateResidencyDataProvider(wlanSdp);
-
-        // Add Easel power entity
-        const std::string easelEntryCountPrefix = "Cumulative count:";
-        const std::string easelTotalTimePrefix = "Cumulative duration msec:";
-        const std::string easelLastEntryPrefix = "Last entry timestamp msec:";
-        std::vector<StateResidencyConfig> easelStateResidencyConfigs = {
-            {.name = "Off",
-             .header = "OFF",
-             .entryCountSupported = true,
-             .entryCountPrefix = easelEntryCountPrefix,
-             .totalTimeSupported = true,
-             .totalTimePrefix = easelTotalTimePrefix,
-             .lastEntrySupported = true,
-             .lastEntryPrefix = easelLastEntryPrefix},
-            {.name = "Active",
-             .header = "ACTIVE",
-             .entryCountSupported = true,
-             .entryCountPrefix = easelEntryCountPrefix,
-             .totalTimeSupported = true,
-             .totalTimePrefix = easelTotalTimePrefix,
-             .lastEntrySupported = true,
-             .lastEntryPrefix = easelLastEntryPrefix},
-            {.name = "Suspend",
-             .header = "SUSPEND",
-             .entryCountSupported = true,
-             .entryCountPrefix = easelEntryCountPrefix,
-             .totalTimeSupported = true,
-             .totalTimePrefix = easelTotalTimePrefix,
-             .lastEntrySupported = true,
-             .lastEntryPrefix = easelLastEntryPrefix}};
-        sp<GenericStateResidencyDataProvider> easelSdp =
-            new GenericStateResidencyDataProvider("/d/mnh_sm/power_stats");
-
-        uint32_t easelId = service->addPowerEntity("Easel", PowerEntityType::SUBSYSTEM);
-        easelSdp->addEntity(
-            easelId, PowerEntityConfig("Easel Subsystem Power Stats", easelStateResidencyConfigs));
-
-        service->addStateResidencyDataProvider(easelSdp);
-    }
-
     // Add Power Entities that require the Aidl data provider
     sp<AidlStateResidencyDataProvider> aidlSdp = new AidlStateResidencyDataProvider();
-    uint32_t citadelId = service->addPowerEntity("Citadel", PowerEntityType::SUBSYSTEM);
-    aidlSdp->addEntity(citadelId, "Citadel", {"Last-Reset", "Active", "Deep-Sleep"});
 
     auto serviceStatus = android::defaultServiceManager()->addService(
         android::String16("power.stats-vendor"), aidlSdp);
