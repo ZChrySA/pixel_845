784d404aeca1f89a3808e555df4ed5d6bb99724b
Arseniy Graur <argraur@gmail.com>
2020-08-19 13:54:29 +0200

    sdm845-common: Update telephony to R

--- device.mk
+++ device.mk
@@ -326,19 +326,6 @@ PRODUCT_PACKAGES += \
     sensors.sdm845 \
     libsensorndkbridge
 
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
