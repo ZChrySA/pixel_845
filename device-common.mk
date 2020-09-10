1ecbab24d64521a16f113084809adb67d3219dac
Arseniy Graur <argraur@gmail.com>
2020-09-10 10:43:38 +0000

    dipper: Enable eBPF
    Signed-off-by: Arseniy Graur <argraur@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -44,6 +44,10 @@ persist.bluetooth.a2dp_offload.disabled=false
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac
 
+# BPF
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.ebpf.supported=true
+
 # whitelisted app
 PRODUCT_COPY_FILES += \
     device/xiaomi/dipper/qti_whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/qti_whitelist.xml
