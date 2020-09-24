64086f53b32f3be006c635f6371b1c3507b50d5a
Arseniy Graur <argraur@gmail.com>
2020-09-24 22:12:21 +0200

    dipper: Inline UICC NFC with blueline.
    Signed-off-by: Arseniy Graur <argraur@gmail.com>

--- nfc/nfc.mk
+++ nfc/nfc.mk
@@ -22,6 +22,12 @@ PRODUCT_PACKAGES += \
     SecureElement
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.dipper.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
