962741eea487d00e10aa549fa06df3afaa29a7ca
Chenyang Zhong <zhongcy95@gmail.com>
2020-12-31 01:58:58 -0500

    sdm845-common: light: adapt aidl Light hal for xiaomi_sdm845
    Note that screen backlight brightness on sdm845 is currently
    controlled by hwc, not by Light HAL. The BACKLIGHT type exists
    in the supported LightType vector to avoid issues from android
    framework.
    
    Due to how populateAvailableLightsFromAidl() works
    in LightsService, non-supported Light devices will be populated
    as NULL, which is different from the behavior of the HIDL equivalent
    populateAvailableLightsFromHidl() where all types of light devices
    are populated as not NULL. As a result, with aidl Light HAL,
    LocalDisplayDevice does not call setBrightness() to set display
    brightness at all and leaves display at max brightness all the
    time.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -400,7 +400,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm845
+    android.hardware.lights-service.xiaomi_sdm845
 
 # Media
 PRODUCT_PACKAGES += \
