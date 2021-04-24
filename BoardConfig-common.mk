d445e3acb2ece1a1642e57f706439a7646328a01
Bruno Martins <bgcngm@gmail.com>
2021-04-24 15:48:15 +0100

    sdm845-common: Build disabled VBMeta image for all targets
    This was never needed for any mi845 target besides perseus
    (all the rest seem to have just empty partitions),
    but should be set here so it fixes signing builds with own keys.
    
    Change-Id: Idded651f3b23535581a555bc2a27409489d8da1b

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -167,6 +167,15 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/xiaomi/dipper-sepolicy/dipper-sepolicy.mk
 
+# Android Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
 # Sound Trigger
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 SOUND_TRIGGER_FEATURE_LPMA_ENABLED := true
